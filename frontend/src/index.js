d4b070e89df3199ee483251b93700d808e86c597
github username <metelskyi88@gmail.com>
2019-03-04 12:49:04 -0500

    fixed gitignore

---
+++ frontend/src/index.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded',() =>{
+  fetchUserInfo()
+})
+
+function fetchUserInfo() {
+fetch(`http://localhost:3000/users/1`)
+.then(resp => resp.json())
+.then(userData => console.log(userData))
+}
