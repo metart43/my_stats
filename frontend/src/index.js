631a1f5c25d9aa7fb7578153d55b12656a00c60b
Ross Jones <rossmosis.jones@gmail.com>
2019-03-04 16:48:27 -0500

    adding user-page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,8 +2,26 @@ document.addEventListener('DOMContentLoaded',() =>{
   fetchUserInfo()
 })
 
+// Returns User URL
+function getUserUrl() {
+  return 'http://localhost:3000/users/1'
+}
+
+// Grab user from API
 function fetchUserInfo() {
-fetch(`http://localhost:3000/users/1`)
-.then(resp => resp.json())
-.then(userData => console.log(userData))
+  fetch(getUserUrl())
+    .then(resp => resp.json())
+    .then(userData => renderUser(userData))
+}
+
+// Renders the user from the JSON
+function renderUser(userData) {
+  let userDiv = document.createElement('div')
+  userDiv.id = "user-info"
+
+  let userName = document.createElement('h2')
+  userName.id = "user-name"
+  userName.innerText = userData.name
+
+  console.log(userData, userDiv, userName)
 }
