c22e79f819ea3da2336aa4ecc815f0434bba7e2b
github username <metelskyi88@gmail.com>
2019-03-06 14:09:05 -0500

    backend team create/update works

--- frontend/src/Team.js
+++ frontend/src/Team.js
@@ -1,5 +1,15 @@
 class Team {
-  constructor() {
+  constructor(team) {
+    this.id = team.id
+    this.name = team.name
+    this.users = team.users
+    Team.all.push(this)
+  }
 
+  render(){
+    let teamName = document.createElement('h2')
+    teamName.innerHTML = `<h2> You Have Successfully Created Team: ${this.name} </h2>`
+    return teamName
   }
 }
+Team.all = []
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -21,43 +21,61 @@ function fetchUserInfo() {
       navButtons(userData)
     })
 }
-
+//Creates Side Menude Buttons
 function navButtons(user) {
 let matchBttn = document.querySelector('#side-button1-match').addEventListener('click', ()=>{currentUserFunc(user).renderMatches()})
 let teamCreateBttn = document.querySelector('#side-button1-new-team').addEventListener('click', ()=>{teamFormCreate(currentUserFunc(user))})
 }
 
-function teamFormCreate(user) {
-  let formHeader = document.createElement('h3')
-  formHeader.innerText = 'Create a New Team:'
+//creates a Team Form
+function teamFormCreate(currentUser) {
   let teamForm = document.createElement('div')
+  let displayContainer = document.querySelector('.display-container')
+  displayContainer.innerHTML = ''
   teamForm.innerHTML += ` <div class="container">
+  <h3>Create a New Team:</h3>
   <br>
   <form id='teamName'>
   <input class="form-control form-control-sm" id="team-name" type="text" placeholder="Team Name">
   <input class="button" id='team-create-button' type="submit">
   </form>
 </div>`
-  document.querySelector('.user_container').append(formHeader,teamForm)
+  displayContainer.appendChild(teamForm)
   let createTeamBttn = document.querySelector('#teamName').addEventListener('submit', () => {
     event.preventDefault()
     let teamName = document.querySelector('#team-name').value
-    createTeam(teamName, user)
+    createTeam(teamName, currentUser)
   })
 }
 
-function createTeam(name, user) {
+//Fetch call to Rails API to create a team
+function createTeam(name, currentUser) {
 fetch('http://localhost:3000/teams',{
       method:"POST",
     	headers: {"Content-Type": "application/json"},
     	body: JSON.stringify( {name: name,
-      user_ids: [user.id]})
+      user_ids: [currentUser.id]})
 })
 .then(resp => resp.json())
-.then(teamData => console.log(teamData))
+.then(teamData => {
+  let teamInstance = new Team (teamData)
+  let teamCard = document.createElement('div')
+  let userContainer = document.querySelector('.display-container')
+  let formContainer = document.querySelector('.container')
+  let teamMateBttn = document.createElement('button')
+  teamMateBttn.innerText = "Add TeamMates"
+  teamMateBttn.id = 'button-mate'
+  teamCard.appendChild(teamInstance.render())
+  userContainer.append(teamCard, teamMateBttn)
+  document.querySelector('#button-mate').addEventListener('click', () =>{
+    fetchUsers(teamData, currentUser)
+  })
+  formContainer.style.display = 'none'
+})
 }
 
-function fetchUsers() {
+//Fetch call to create find a user
+function fetchUsers(teamData, currentUser) {
   let teamMatesSelect = document.createElement('div')
   teamMatesSelect.innerHTML += `<div class="container">
   <br>
@@ -70,7 +88,6 @@ function fetchUsers() {
 document.querySelector('.user_container').append(teamMatesSelect)
 document.querySelector('#teamMate').addEventListener('submit', () =>{
   event.preventDefault()
-  let teamName = document.querySelector('#team-name').value
   let search = document.querySelector('#myInput').value
   fetch('http://localhost:3000/users')
   .then(resp => resp.json())
@@ -80,8 +97,18 @@ document.querySelector('#teamMate').addEventListener('submit', () =>{
         alert("User cannot be found!")
         return
     } else {
-      // createTeam(foundUser, teamName)
+       updateTeam(foundUser, teamData, currentUser)
     }
   })
 })
+}
+
+function updateTeam(foundUser, teamData, currentUser) {
+  debugger
+  fetch(`http://localhost:3000/teams/${teamData.id}`,{
+        method:"PATCH",
+      	headers: {"Content-Type": "application/json"},
+      	body: JSON.stringify( {name: teamData.name,
+        user_ids: [foundUser.id, currentUser.id]})
+  })
 }
