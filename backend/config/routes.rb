fec25af8216edd4d27acd649b593e01d56bde77a
github username <metelskyi88@gmail.com>
2019-03-07 14:55:33 -0500

    tmp/log delete jesus please help us

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, except: [:destroy, :index], param: :name
+  resources :users, except: [:destroy], param: :name
   resources :matches, only: [:create, :destroy, :update]
   resources :heros, only: [:show]
   resources :teams, only: [:show, :create, :update]
